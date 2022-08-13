bbb56820afe9281bf5a693168539f8fd2b6cce06
Jack Sandom <jack.sandom@clearscore.com>
2022-08-13 13:17:40 +0100

    Use OS to return right directory in test

--- starter/test_model.py
+++ starter/test_model.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import pandas as pd
 import sklearn
@@ -6,7 +7,7 @@ from starter.ml.data import process_data
 from starter.ml.model import train_model, compute_model_metrics, inference
 
 # Add code to load in the data.
-data = pd.read_csv("./starter/data/census.csv")
+data = pd.read_csv(os.path.join(os.getcwd(), "starter/data/census.csv"))
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
